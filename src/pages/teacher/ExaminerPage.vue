bc0169cc1f87b8871749f39e0b749e25219bd10c
Zawad Abdullah <1605002@ugrad.cse.buet.ac.bd>
2021-04-22 23:53:43 +0600

    Addded a simple editable table in examiner page

--- src/pages/teacher/ExaminerPage.vue
+++ src/pages/teacher/ExaminerPage.vue
@@ -7,13 +7,61 @@
       <p>
         Internals and Externals will upload and update exxam marks here.
       </p>
+
+      <div class="q-pa-md">
+        <q-table
+          :data="marks"
+          row-key="Student_ID"
+          separator="vertical"
+        >
+          <template v-slot:body="props">
+            <q-tr :props="props">
+              <q-td key="Student_ID" :props="props">
+                {{ props.row.Student_ID }}
+              </q-td>
+              <q-td key="marks" :props="props">
+                <!-- {{ props.row.marks }} -->
+                <q-input type="number" v-model="props.row.marks" dense autofocus />
+                <!-- <q-popup-edit v-model="props.row.marks" title="Upload marks">
+                  <q-input type="number" v-model="props.row.marks" dense autofocus />
+                </q-popup-edit> -->
+              </q-td>
+            </q-tr>
+          </template>
+        </q-table>
+      </div>
     </div>
   </q-page>
 </template>
 
 <script>
+
+let marks = [
+  {
+    "Student_ID": "1605002",
+    "marks": 69,
+  },
+  {
+    "Student_ID": "1605003",
+    "marks": 70,
+  },
+  {
+    "Student_ID": "1605004",
+    "marks": 99,
+  },
+  {
+    "Student_ID": "1605083",
+    "marks": 89,
+  },
+];
+
 export default {
-  name: "ExaminePage"
+  name: "ExaminePage",
+  data() {
+    return {
+      marks,
+    };
+  }
 };
 </script>
 
