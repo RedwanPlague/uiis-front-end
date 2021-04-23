6c143aa2e5fbe1904c5da9f5db10020d617ebc23
Zawad Abdullah <1605002@ugrad.cse.buet.ac.bd>
2021-04-23 17:31:59 +0600

    Created ExaminerTable component to support multiple courses

---
+++ src/components/ExaminerTable.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="q-pa-sm">
+    <h6>{{courseName}}</h6>
+
+    <div class="q-pa-md">
+      <q-table
+        :data="marks"
+        :columns="columns"
+        row-key="student_id"
+        separator="cell"
+      >
+        <template v-slot:body="props">
+          <q-tr :props="props">
+            <q-td key="student_id" :props="props">
+              {{ props.row.student_id }}
+            </q-td>
+            <q-td key="marks" :props="props">
+              <!-- {{ props.row.marks }} -->
+              <q-input type="number" v-model="props.row.marks" autofocus dense />
+            </q-td>
+          </q-tr>
+        </template>
+      </q-table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ExaminerTable',
+  props: {
+    courseName: {
+      type: String,
+      required: true,
+    },
+
+    marks: {
+      type: Array,
+      required: true
+    },
+  },
+
+  data() {
+    return {
+      columns: [
+        {
+          // unique id, identifies column
+          name: "student_id",
+
+          // label that is displayed in the table
+          label: "Student ID",
+          field: "student_id",
+
+          align: "left",
+          sortable: false,
+        },
+        {
+          name: "marks", label: "Marks", field: "marks", align: "left", sortable: false,
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/teacher/ExaminerPage.vue
+++ src/pages/teacher/ExaminerPage.vue
@@ -1,68 +1,72 @@
 <template>
   <q-page padding>
-    <div class="q-pa-sm absolute-center">
-      <h3>
-        Examiner
-      </h3>
-      <p>
-        Internals and Externals will upload and update exxam marks here.
-      </p>
-
-      <div class="q-pa-md">
-        <q-table
-          :data="marks"
-          row-key="Student_ID"
-          separator="vertical"
-        >
-          <template v-slot:body="props">
-            <q-tr :props="props">
-              <q-td key="Student_ID" :props="props">
-                {{ props.row.Student_ID }}
-              </q-td>
-              <q-td key="marks" :props="props">
-                <!-- {{ props.row.marks }} -->
-                <q-input type="number" v-model="props.row.marks" dense autofocus />
-                <!-- <q-popup-edit v-model="props.row.marks" title="Upload marks">
-                  <q-input type="number" v-model="props.row.marks" dense autofocus />
-                </q-popup-edit> -->
-              </q-td>
-            </q-tr>
-          </template>
-        </q-table>
-      </div>
-    </div>
+    <ExaminerTable v-for="course in courses" :key="course.courseName" v-bind="course"></ExaminerTable>
   </q-page>
 </template>
 
 <script>
 
-let marks = [
-  {
-    "Student_ID": "1605002",
-    "marks": 69,
-  },
-  {
-    "Student_ID": "1605003",
-    "marks": 70,
-  },
-  {
-    "Student_ID": "1605004",
-    "marks": 99,
-  },
-  {
-    "Student_ID": "1605083",
-    "marks": 89,
-  },
-];
-
 export default {
   name: "ExaminePage",
+  components: {
+    ExaminerTable: () => import('../../components/ExaminerTable.vue'),
+  },
   data() {
     return {
-      marks,
+      courses: null
     };
+  },
+
+  created() {
+    // API call
+
+    this.courses = [
+      {
+        courseName: "CSE311",
+        marks: [
+          {
+            student_id: "1605002",
+            marks: 34,
+          },
+          {
+            student_id: "1605003",
+            marks: 31,
+          },
+          {
+            student_id: "1605004",
+            marks: 37,
+          },
+        ],
+      },
+      {
+        courseName: "CSE313",
+        marks: [
+          {
+            student_id: "1605002",
+            marks: 43,
+          },
+          {
+            student_id: "1605003",
+            marks: 13,
+          },
+          {
+            student_id: "1605004",
+            marks: 73,
+          },
+        ],
+      },
+    ]
   }
 };
 </script>
 
-<style scoped></style>
+<style>
+input[type="number"]::-webkit-outer-spin-button, input[type="number"]::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+}
+ 
+input[type="number"] {
+    -moz-appearance: textfield;
+}
+</style>
