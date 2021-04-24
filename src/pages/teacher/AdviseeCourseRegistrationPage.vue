5d39dee48126d3938e88f73258455c24c3f91eb7
FromSaffroncity <yasar199510@gmail.com>
2021-04-24 15:34:29 +0600

    Some development in Advisor side

--- src/pages/teacher/AdviseeCourseRegistrationPage.vue
+++ src/pages/teacher/AdviseeCourseRegistrationPage.vue
@@ -1,19 +1,178 @@
 <template>
   <q-page padding>
-    <div class="q-pa-sm absolute-center">
-      <h3>
-        Advisee Course Registration Approval
-      </h3>
-      <p>
-        Advisor will approve advisees' course registration application.
-      </p>
+    <div class="q-pa-md">
+      <q-card bordered class="bg-grey-2">
+        <q-tabs
+          v-model="tab"
+          no-caps
+          class="bg-primary text-white shadow-7"
+          align="justify"
+          narrow-indicator
+        >
+          <q-tab name="registeredStudents" label="Registered Students" />
+          <q-tab name="waitingForHeadApproval" label="Waiting for Head's Approval" />
+          <q-tab name="waitingForAdvisorApproval" label="Waiting for Head's Approval" />
+          <q-tab name="notAppliedForRegistration" label="Not Applied for Registration" />
+        </q-tabs>
+
+        <q-tab-panels v-model="tab" animated>
+          <q-tab-panel name="registeredStudents">
+            <div class="q-gutter-sm">
+              <q-btn
+                color="primary"
+                v-for="advisee in advisees"
+                :key="advisee.studentID"
+                v-bind="advisee"
+              >
+                {{ advisee.studentID }}
+              </q-btn>
+            </div>
+          </q-tab-panel>
+
+          <q-tab-panel name="waitingForHeadApproval">
+            <div class="q-gutter-sm">
+              <q-btn
+                color="primary"
+                v-for="advisee in advisees"
+                :key="advisee.studentID"
+                v-bind="advisee"
+              >
+                {{ advisee.studentID }}
+              </q-btn>
+            </div>
+          </q-tab-panel>
+
+          <q-tab-panel name="waitingForAdvisorApproval">
+            <q-table
+              title="Waiting for Adviser's Approval" bordered class="bg-grey-2" :data="rows" :columns="columns" row-key="studentID"
+              :pagination.sync="pagination" hide-pagination
+              :selected-rows-label="getSelectedString" selection="multiple" :selected.sync="selected"
+            />
+
+            <div class="row justify-center q-mt-md">
+              <q-pagination
+                v-model="pagination.page"
+                color="primary"
+                :max="pagesNumber"
+                size="sm"
+              />
+            </div><br />
+
+            <div class="row">
+              <q-space />
+              <q-btn class="btn-fixed-width" color="primary" label="Submit" />
+            </div>
+          </q-tab-panel>
+
+          <q-tab-panel name="notAppliedForRegistration">
+            <div class="q-gutter-sm">
+              <q-btn
+                color="primary"
+                v-for="advisee in advisees"
+                :key="advisee.studentID"
+                v-bind="advisee"
+              >
+                {{ advisee.studentID }}
+              </q-btn>
+            </div>
+          </q-tab-panel>
+        </q-tab-panels>
+      </q-card>
     </div>
   </q-page>
 </template>
 
 <script>
+let advisees = [];
+
+for(let i=0; i<30; i++) {
+  advisees[i] = { studentID: 1605001 + i };
+}
+
 export default {
-  name: "AdvisorPage"
+  name: "AdviseeCourseRegistrationPage",
+
+  data () {
+    return {
+      /* necessary data */
+      advisees,
+
+      /* by default location for q-tab */
+      tab: "registeredStudents",
+
+      studentInfo: false,
+
+      /* for multiple selection */
+      selected: [],
+
+      /* for pagination */
+      pagination: {
+        sortBy: 'studentID',
+        descending: false,
+        page: 1,
+        rowsPerPage: 2
+        /* rowsNumber: xx if getting data from a server */
+      },
+
+      /* for tabulation */
+      columns: [
+        {
+          name: 'studentID',
+          required: true,
+          label: 'Student ID',
+          align: 'left',
+          field: row => row.studentID,
+          format: val => `${val}`,
+          sortable: true
+        },
+        {
+          name: 'name',
+          align: 'left',
+          label: 'Name',
+          field: 'name',
+          sortable: true
+        }
+      ],
+      rows: [
+        {
+          studentID: '1605002',
+          name: 'Zawad'
+        },
+        {
+          studentID: '1605003',
+          name: 'Bishwa'
+        },
+        {
+          studentID: '1605004',
+          name: 'Redwan'
+        },
+        {
+          studentID: '1605010',
+          name: 'Rakib'
+        },
+        {
+          studentID: '1605023',
+          name: 'Sahil'
+        },
+        {
+          studentID: '1605024',
+          name: 'Mahir'
+        }
+      ]
+    };
+  },
+
+  computed: {
+    pagesNumber () {
+      return Math.ceil(this.rows.length / this.pagination.rowsPerPage);
+    }
+  },
+
+  methods: {
+    getSelectedString () {
+      return this.selected.length === 0 ? '' : `${this.selected.length} record${this.selected.length > 1 ? 's' : ''} selected of ${this.rows.length}`;
+    }
+  }
 };
 </script>
 
--- src/pages/teacher/AdviseeInformationPage.vue
+++ src/pages/teacher/AdviseeInformationPage.vue
@@ -1,38 +1,91 @@
 <template>
   <q-page padding>
-    <div class="q-pa-sm">
-      <q-markup-table>
-        <thead>
-          <tr>
-            <th class="text-left">Student ID</th>
-            <th class="text-left">Name</th>
-          </tr>
-        </thead>
+    <div class="q-pa-md">
+      <q-table
+        title="Advisees" :data="rows" :columns="columns" row-key="studentID"
+        :pagination.sync="pagination" hide-pagination
+      />
 
-        <tbody>
-          <tr>
-            <td class="text-left">1605002</td>
-            <td class="text-left">Zawad</td>
-          </tr>
-
-          <tr>
-            <td class="text-left">1605023</td>
-            <td class="text-left">Sahil</td>
-          </tr>
-
-          <tr>
-            <td class="text-left">1605024</td>
-            <td class="text-left">Mahir</td>
-          </tr>
-        </tbody>
-      </q-markup-table>
+      <div class="row justify-center q-mt-md">
+        <q-pagination
+          v-model="pagination.page"
+          color="secondary"
+          :max="pagesNumber"
+          size="sm"
+        />
+      </div>
     </div>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: "AdvisorPage"
+  name: "AdviseeInformationPage",
+
+  data () {
+    return {
+      /* for pagination */
+      pagination: {
+        sortBy: 'studentID',
+        descending: false,
+        page: 1,
+        rowsPerPage: 10
+        /* rowsNumber: xx if getting data from a server */
+      },
+
+      /* for tabulation */
+      columns: [
+        {
+          name: 'studentID',
+          required: true,
+          label: 'Student ID',
+          align: 'left',
+          field: row => row.studentID,
+          format: val => `${val}`,
+          sortable: true
+        },
+        {
+          name: 'name',
+          align: 'left',
+          label: 'Name',
+          field: 'name',
+          sortable: true
+        }
+      ],
+      rows: [
+        {
+          studentID: '1605002',
+          name: 'Zawad'
+        },
+        {
+          studentID: '1605003',
+          name: 'Bishwa'
+        },
+        {
+          studentID: '1605004',
+          name: 'Redwan'
+        },
+        {
+          studentID: '1605010',
+          name: 'Rakib'
+        },
+        {
+          studentID: '1605023',
+          name: 'Sahil'
+        },
+        {
+          studentID: '1605024',
+          name: 'Mahir'
+        }
+      ]
+    };
+  },
+
+  computed: {
+    pagesNumber () {
+      return Math.ceil(this.rows.length / this.pagination.rowsPerPage);
+    }
+  }
 };
 </script>
 
